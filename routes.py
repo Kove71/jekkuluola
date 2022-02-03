f260f349f3aef48e2cede861344aa21406bb46fd
Kosti Vesterinen <kosti.vesterinen@helsinki.fi>
2022-02-03 19:53:39 +0200

    käyttäjät ja session lisätty

--- routes.py
+++ routes.py
@@ -1,11 +1,13 @@
 from app import app
 from flask import render_template, request, redirect
 from services.jokes import get_jokes, add_joke
+from services.users import create_user, get_session_user, user_login, user_logout
 
 @app.route("/")
 def index():
     jokes = get_jokes()
-    return render_template("index.html", jokes=jokes)
+    user_data = get_session_user()
+    return render_template("index.html", jokes=jokes, user=user_data)
 
 @app.route("/newjoke", methods=["GET", "POST"])
 def new_joke():
@@ -15,3 +17,31 @@ def new_joke():
         joke_content = request.form["joke"]
         add_joke(joke_content)
         return redirect("/")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        create_user(username, password)
+        return redirect("/login")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if user_login(username, password):
+            return redirect("/")
+        else:
+            return redirect("/login")
+
+@app.route("/logout")
+def logout():
+    user_logout()
+    return redirect("/")
---
+++ services/users.py
@@ -0,0 +1,39 @@
+from flask import session
+from werkzeug.security import generate_password_hash, check_password_hash
+from db import db
+
+
+def create_user(username, password):
+    hashed_pw = generate_password_hash(password)
+    sql = ("INSERT INTO users (username, password, admin) VALUES (:username, :password, FALSE)")
+    db.session.execute(sql, {
+        "username": username,
+        "password": hashed_pw
+    })
+    db.session.commit()
+
+
+def user_login(username, password):
+    sql = "SELECT id, password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username": username})
+    user = result.fetchone()
+    if not user:
+        return False
+    else:
+        if check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            return True
+        else:
+            return False
+
+def user_logout():
+    del session["user_id"]
+
+def get_session_user():
+    try:
+        user_id = session["user_id"]
+        sql = "SELECT username, admin FROM users WHERE id=:user_id"
+        result = db.session.execute(sql, {"user_id":user_id})
+        return result.fetchone()
+    except Exception:
+        return False
